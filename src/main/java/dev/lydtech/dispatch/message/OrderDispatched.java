e8dea3760d55a1414bcd4af92cafb6cbb7e29046
raw <ryaboff99@gmail.com>
2024-01-15 20:50:18 +0200

    Add OrderDispatched event POJO
    * Add OrderDispatched.java

---
+++ src/main/java/dev/lydtech/dispatch/message/OrderDispatched.java
@@ -0,0 +1,19 @@
+package dev.lydtech.dispatch.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDispatched {
+
+    UUID orderId;
+    UUID processedById;
+    String notes;
+}
